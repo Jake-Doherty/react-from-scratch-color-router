b34f7b15269d0d96a23ac2a1442028eed72e4b4a
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-24 11:33:48 -0800

    use isActive to change color of Navlink when navlink is selected

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,34 @@
 import React from 'react';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 export default function Nav() {
   return (
     <nav>
-      {/* <NavLink>Red</NavLink>
-      <NavLink>Green</NavLink>
-      <NavLink>Blue</NavLink> */}
+      <NavLink
+        to="/rgb/255/0/0"
+        style={(isActive) => ({
+          color: isActive ? 'red' : 'white',
+        })}
+      >
+        Red
+      </NavLink>
+      <NavLink
+        to="/rgb/0/255/0"
+        style={(isActive) => ({
+          color: isActive ? 'green' : 'white',
+        })}
+      >
+        Green
+      </NavLink>
+      <NavLink
+        to="/rgb/0/0/255"
+        style={(isActive) => ({
+          color: isActive ? 'blue' : 'white',
+        })}
+      >
+        Blue
+      </NavLink>
     </nav>
   );
 }
