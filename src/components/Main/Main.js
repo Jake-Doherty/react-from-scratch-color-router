dac0de656d9342ec3c9fc18a32dfda768b1a4fad
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-24 11:30:54 -0800

    import and mount RGB component in Switch Route

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Nav from '../Nav/Nav.js';
+import RGB from '../RGB/RGB.js';
 import './Main.css';
 
 export default function Main() {
@@ -8,7 +9,7 @@ export default function Main() {
     <main>
       <Nav />
       <Switch>
-        <Route path="/rgb" />
+        <Route path="/rgb/:r/:g/:b" component={RGB} />
       </Switch>
     </main>
   );
